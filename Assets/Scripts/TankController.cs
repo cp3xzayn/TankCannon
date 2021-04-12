51590c05f0d923b5c0baae209076f05aa660cf3a
h.kato <cp3xzayn@gmail.com>
2021-04-13 04:27:32 +0900

    戦車の移動をJoyStickを用いて実装した。

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 2;
+    [SerializeField] FloatingJoystick m_floatingJoystick = null;
+    Rigidbody rb;
+    private Vector3 m_direction;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (m_direction != Vector3.zero)
+        {
+            transform.localRotation = Quaternion.LookRotation(m_direction);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        m_direction = Vector3.forward * m_floatingJoystick.Vertical + Vector3.right * m_floatingJoystick.Horizontal;
+        rb.velocity = m_direction * m_speed;
+    }
+}
