e29b7466841bca2b56b1839ad6ef12c4d90ccac4
h.kato <cp3xzayn@gmail.com>
2021-04-15 15:44:27 +0900

    戦車をドラッグアンドドロップで移動できるようにした。Gridポジションにセットするようにした。

--- Assets/Scripts/TankGenerator.cs
+++ Assets/Scripts/TankGenerator.cs
@@ -5,29 +5,47 @@ using UnityEngine;
 public class TankGenerator : MonoBehaviour
 {
     /// <summary> 生成する戦車のオブジェクト </summary>
-    [SerializeField] GameObject m_tank = null;
-    [SerializeField] GameObject m_targetMarker = null;
-
-    void Start()
-    {
-        m_targetMarker.SetActive(false);
-    }
+    [SerializeField] GameObject[] m_tank = null;
+    /// <summary> オブジェクトをつかんでいるか判定する </summary>
+    bool isGrabbing;
+    /// <summary> つかんでいる戦車がどれか判定するための変数 </summary>
+    int m_tankIndex;
 
     void Update()
     {
         if (GameManager.Instance.NowGameState == GameState.Prepare)
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.tag == "Tank")
+                    {
+                        if (hit.collider.gameObject == m_tank[0]) m_tankIndex = 0;
+                        if (hit.collider.gameObject == m_tank[1]) m_tankIndex = 1;
+                        if (hit.collider.gameObject == m_tank[2]) m_tankIndex = 2;
+                        Debug.Log("戦車をつかんでいます。");
+                        isGrabbing = true;
+                    }
+                }
+            }
+        }
+
+        if (isGrabbing)
+        {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                if (hit.collider.gameObject.tag == "Field")
+                if (hit.collider.tag == "Field")
                 {
-                    MarkerAction(hit.point);
-                    if (Input.GetButtonDown("Fire1"))
-                    {
-                        InstantiateTank(hit.point);
-                    }
+                    m_tank[m_tankIndex].transform.position = TankSetPosition(hit.point);
+                }
+                if (Input.GetMouseButtonUp(0))
+                {
+                    isGrabbing = false;
                 }
             }
         }
@@ -42,63 +60,20 @@ public class TankGenerator : MonoBehaviour
     [SerializeField] int m_minZ = 3;
     /*-----------------------------------------*/
 
-    /// <summary>
-    /// 戦車を生成する
-    /// </summary>
-    /// <param name="hitPos"></param>
-    void InstantiateTank(Vector3 hitPos)
-    {
-        // 戦車をセットする場所のGrid座標
-        Vector3Int setPosGrid = VectorInfo.TankSetPositionToGrid(hitPos);
-        // 設置するためにVector3に変換
-        Vector3 setPos = TankSetPosition(setPosGrid);
-        // 戦車が生成可能範囲が選択された場合（現状IndexOutOfrangeが出てしまってる　＝＞　修正する必要あり）
-        // 戦車設置可能かを判断し、可能なら戦車を生成する。
-        if (VectorInfo.mapInfo[setPosGrid.x - m_minX, setPosGrid.z - m_minZ] == 0)
-        {
-            VectorInfo.mapInfo[setPosGrid.x - m_minX, setPosGrid.z - m_minZ] = 1;
-            GameObject obj = Instantiate(m_tank, setPos, Quaternion.identity);
-            obj.transform.parent = transform;
-        }
-        else
-        {
-            Debug.Log("すでに設置済みです。ここには設置できません。");
-        }
-    }
 
     /// <summary>
-    /// 取得した戦車の設置位置のVector3Intの情報をVector3に変換する
+    /// 戦車の位置を一度Vector3Intに変換し、設置するためにもう一度Vector3に変換する
     /// </summary>
     /// <param name="tankPosToGrid"></param>
     /// <returns></returns>
-    private Vector3 TankSetPosition(Vector3Int tankPosToGrid)
+    private Vector3 TankSetPosition(Vector3 raycastPos)
     {
+        // GridポジションをVector3Intで取得する。
+        Vector3Int tankPosToGrid = VectorInfo.TankSetPositionToGrid(raycastPos);
+        // 高さが必要なため、もう一度Vector3に戻す
         int x = tankPosToGrid.x;
         int z = tankPosToGrid.z;
-        Vector3 tankSetPos = new Vector3(x, 0.3f, z);　// 戦車の設置の高さ（0.3f）;
+        Vector3 tankSetPos = new Vector3(x, 0.23f, z);　// 戦車の設置の高さ（0.23f）;
         return tankSetPos;
-    }
-
-    /// <summary>
-    /// Markerのポジションを移動する
-    /// </summary>
-    /// <param name="hitPos"></param>
-    void MarkerAction(Vector3 hitPos)
-    {
-        if (hitPos.x >= m_minX && hitPos.x <= m_maxX)
-        {
-            if (hitPos.z >= m_minZ && hitPos.z <= m_maxZ)
-            {
-                m_targetMarker.SetActive(true);
-                Vector3Int setPosGrid = VectorInfo.TankSetPositionToGrid(hitPos);
-                Vector3 markerPos = new Vector3(setPosGrid.x, 0.0001f, setPosGrid.z); // TargetMarkerの座標
-                m_targetMarker.transform.position = markerPos;
-            }
-        }
-        else
-        {
-            m_targetMarker.SetActive(false);
-        }
-        
     }
 }
