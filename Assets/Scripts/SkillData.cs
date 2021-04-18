3874f1723d2c4bf6e8e3e071633449d75a915600
cp3xzayn <cp3xzayn@gmail.com>
2021-04-18 16:44:59 +0900

    Skillを選択できる画面を作成した。スキルの情報を保持しているScriptableObjectを作成した。

---
+++ Assets/Scripts/SkillData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SkillData", menuName = "ScriptableObjects/CreateSkillDataAsset")]
+public class SkillData : ScriptableObject
+{
+    public List<SkillParam> SkillParamList = new List<SkillParam>();
+}
+
+/// <summary>
+/// スキルの情報を保持するクラス
+/// </summary>
+[System.Serializable]
+public class SkillParam
+{
+    /// <summary> 効果の名前 </summary>
+    [Header("効果の名前")] public string m_skillName;
+    /// <summary> 効果の画像 </summary>
+    [Header("効果の画像")] public Sprite m_skillImage;
+    /// <summary> 効果を得るために必要なコスト </summary>
+    [Header("必要コスト")] public int m_needCost;
+}
---
+++ Assets/Scripts/SkillDataSet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillDataSet : MonoBehaviour
+{
+    [SerializeField] SkillData m_skillData = null;
+
+    [SerializeField] Text[] m_effectNameText = null;
+    [SerializeField] Image[] m_effectImage = null;
+    [SerializeField] Text[] m_needCost = null;
+
+    void Start()
+    {
+        SetSkillData();
+    }
+
+    void SetSkillData()
+    {
+        Debug.Log("q");
+        for (int i = 0; i < m_skillData.SkillParamList.Count; i++)
+        {
+            Debug.Log("a");
+            m_effectNameText[i].text = m_skillData.SkillParamList[i].m_skillName;
+            m_effectImage[i].sprite = m_skillData.SkillParamList[i].m_skillImage;
+            m_needCost[i].text = "必要コスト" + m_skillData.SkillParamList[i].m_needCost;
+        }
+    }
+}
