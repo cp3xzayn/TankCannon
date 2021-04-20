325a42cd93da62467b10fc88fdde7edac5a1b7f9
cp3xzayn <cp3xzayn@gmail.com>
2021-04-20 09:34:37 +0900

    Skillにコストを設定し、必要コストが足りなかったら、SkillButtonを押せないようにした。

--- Assets/Scripts/Skill/SkillUPManager.cs
+++ Assets/Scripts/Skill/SkillUPManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillUPManager : MonoBehaviour
 {
@@ -10,16 +11,60 @@ public class SkillUPManager : MonoBehaviour
     float m_shootRangeUP = 10f;
     /// <summary> 現在の射程範囲 </summary>
     float m_shootRange;
-    /// <summary> 生成間隔の上がり値(割る) </summary>
-    int m_shootTimeUP = 2;
+    /// <summary> 生成間隔の上がり値(減らす) </summary>
+    float m_shootTimeUP = -0.5f;
     /// <summary> 現在の弾生成間隔 </summary>
     float m_shootTime;
 
+    /// <summary> 所持コスト </summary>
+    [SerializeField] private int m_cost = 20;
+
+    /// <summary> 所持コストを表示するTextのGameObject </summary>
+    [SerializeField] GameObject m_costObj = null;
+    /// <summary> 所持コストを表示するText </summary>
+    Text m_costText;
+
+    [SerializeField] Button[] m_skillButton = null;
+
+    /// <summary> SkillData </summary>
+    [SerializeField] SkillData m_skillData = null;
+
     void Start()
     {
+        m_costText = m_costObj.GetComponent<Text>();
+        // それぞれのステータスを初期化
         m_bulletDamage = TankStatus.BulletDamage;
         m_shootRange = TankStatus.ShootVelocity;
         m_shootTime = TankStatus.ShootTime;
+    }
+
+    void Update()
+    {
+        m_costText.text = "所持コスト:" + m_cost;
+        ButtonInteractable();
+    }
+
+    void ButtonInteractable()
+    {
+        for (int i = 0; i < m_skillButton.Length; i++)
+        {
+            if (m_cost >= m_skillData.SkillParamList[i].m_needCost)
+            {
+                m_skillButton[i].interactable = true;
+            }
+            else
+            {
+                m_skillButton[i].interactable = false;
+            }
+        }
+    }
+
+    /// <summary> 射程Upが押されたときの処理 </summary>
+    public void OnClickVelocityUP()
+    {
+        m_shootRange += m_shootRangeUP;
+        TankStatus.ShootVelocity = m_shootRange;
+        m_cost -= m_skillData.SkillParamList[0].m_needCost;
     }
 
     /// <summary> ダメージUpが押されたときの処理 </summary>
@@ -27,17 +72,14 @@ public class SkillUPManager : MonoBehaviour
     {
         m_bulletDamage += m_bulletDamageUP;
         TankStatus.BulletDamage = m_bulletDamage;
+        m_cost -= m_skillData.SkillParamList[1].m_needCost;
     }
-    /// <summary> 射程Upが押されたときの処理 </summary>
-    public void OnClickVelocityUP()
-    {
-        m_shootRange += m_shootRangeUP;
-        TankStatus.ShootVelocity = m_shootRange;
-    }
+
     /// <summary> 生成間隔Upが押されたときの処理 </summary>
     public void OnClickShootTimeUP()
     {
-        m_shootTime /= m_shootTimeUP;
+        m_shootTime += m_shootTimeUP;
         TankStatus.ShootTime = m_shootTime;
+        m_cost -= m_skillData.SkillParamList[2].m_needCost;
     }
 }
